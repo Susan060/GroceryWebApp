01e45381333032c77444253c711b485d3ab87861
Susan060 <sushantadhikari060@gmail.com>
2025-12-09 23:36:20 +0545

    Creating edit role & mobile page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,29 @@
+import { auth } from "@/auth";
+import EditRoleMobile from "@/components/EditRoleMobile";
+import connectDb from "@/lib/db";
+import User from "@/models/user.model";
+import { redirect } from "next/navigation";
 import Image from "next/image";
 
-export default function Home() {
-  return (
-<div></div>
+async function Home() {
+await connectDb()
+const session=await auth()
+const user=await User.findById(session?.user?.id)
+if(!user)
+{
+  redirect("/login")
+}
+
+const inComplete=!user.mobile||!user.role||(!user.mobile && user.role=="user")
+if(inComplete)
+{
+  return <EditRoleMobile/>
+}
+return (
+<div>
+
+</div>
   );
 }
+
+export default Home
