58a2ad8da91b45054e093b08d5e84b4c87c0bb74
Susan060 <sushantadhikari060@gmail.com>
2025-12-15 18:24:18 +0545

    Creating Cart Page

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -25,7 +25,7 @@ function Nav({ user }: { user: IUser }) {
     const [searchBarOpen, setSearchBarOpen] = useState(false)
     const profileDropDown = useRef<HTMLDivElement>(null)
     const [menuOpen, setMenuOpen] = useState(false)
-    const {cartData}=useSelector((state:RootState)=>state.cart)
+    const { cartData } = useSelector((state: RootState) => state.cart)
     useEffect(() => {
         const handleClickOutside = (e: MouseEvent) => {
             if (profileDropDown.current && !profileDropDown.current.contains(e.target as Node)) {
@@ -81,7 +81,7 @@ function Nav({ user }: { user: IUser }) {
                 {user.role == "user" && <>                <div className='bg-white rounded-full w-11 h-11 flex items-center justify-center shadow-md hover:scale-105 transition md:hidden' onClick={() => setSearchBarOpen((prev) => !prev)}>
                     <Search className='text-green-600 w-6 h-6' />
                 </div>
-                    <Link href={""} className='relative bg-white rounded-full w-11 h-11 flex items-center justify-center shadow-md hover:scale-105 transition'>
+                    <Link href={"/user/cart"} className='relative bg-white rounded-full w-11 h-11 flex items-center justify-center shadow-md hover:scale-105 transition'>
                         <ShoppingCartIcon className='text-green-600 w-6 h-6' />
                         <span className='absolute -top-1 -right-1 bg-red-500 text-white text-xs w-5 h-5 flex
                     items-center justify-center rounded-full font-semibold shadow'>{cartData.length}</span></Link></>}
--- src/redux/cartSlice.ts
+++ src/redux/cartSlice.ts
@@ -2,49 +2,64 @@ import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import mongoose from "mongoose";
 
 interface IGrocery {
-    _id?: mongoose.Types.ObjectId,
+    _id: mongoose.Types.ObjectId,
     name: string,
     category: string,
     price: string,
     unit: string,
-    quantity:number,
+    quantity: number,
     image: string,
     createdAt?: Date,
     updatedAt?: Date
 }
-interface ICartSlice{
-   cartData: IGrocery[]
-    
+interface ICartSlice {
+    cartData: IGrocery[],
+    subTotal: number,
+    deliveryFee: number,
+    finalTotal: number
 }
-const initialState:ICartSlice={
-   cartData:[] 
-    
-
+const initialState: ICartSlice = {
+    cartData: [],
+    subTotal: 0,
+    deliveryFee: 40,
+    finalTotal: 40
 }
-const cartSlice=createSlice({
-    name:"cart",
+const cartSlice = createSlice({
+    name: "cart",
     initialState,
-    reducers:{
-       addToCart:(state,action:PayloadAction<IGrocery>)=>{
-        state.cartData?.push(action.payload)
-       },
-       increaseQuantity:(state,action:PayloadAction<mongoose.Types.ObjectId>)=>{
-        const item=state.cartData.find(i=>i._id==action.payload)
-        if(item){
-            item.quantity=item.quantity+1
+    reducers: {
+        addToCart: (state, action: PayloadAction<IGrocery>) => {
+            state.cartData?.push(action.payload)
+            cartSlice.caseReducers.calculateTotals(state)
+        },
+        increaseQuantity: (state, action: PayloadAction<mongoose.Types.ObjectId>) => {
+            const item = state.cartData.find(i => i._id == action.payload)
+            if (item) {
+                item.quantity = item.quantity + 1
+            }
+            cartSlice.caseReducers.calculateTotals(state)
+        },
+        decreaseQuantity: (state, action: PayloadAction<mongoose.Types.ObjectId>) => {
+            const item = state.cartData.find(i => i._id == action.payload)
+            if (item?.quantity && item.quantity > 1) {
+                item.quantity = item.quantity - 1
+            } else {
+                state.cartData = state.cartData.filter(i => i._id !== action.payload)
+            }
+            cartSlice.caseReducers.calculateTotals(state)
+        },
+        removeFromCart: (state, action: PayloadAction<mongoose.Types.ObjectId>) => {
+            state.cartData = state.cartData.filter(i => i._id !== action.payload)
+            cartSlice.caseReducers.calculateTotals(state)
+        },
+        calculateTotals:(state)=>{
+            state.subTotal=state.cartData.reduce((sum,item)=>sum +Number(item.price)*item.quantity,0)
+            state.deliveryFee=state.subTotal>100?0:40
+            state.finalTotal=state.subTotal+state.deliveryFee
         }
-       },
-       decreaseQuantity:(state,action:PayloadAction<mongoose.Types.ObjectId>)=>{
-        const item=state.cartData.find(i=>i._id==action.payload)
-        if(item?.quantity && item.quantity>1){
-            item.quantity=item.quantity-1
-        }else{
-          state.cartData= state.cartData.filter(i=>i._id!==action.payload)
-        }
-       }
     }
 })
 
-export const {addToCart,increaseQuantity,decreaseQuantity}=cartSlice.actions
+export const { addToCart, increaseQuantity, decreaseQuantity, removeFromCart,calculateTotals } = cartSlice.actions
 export default cartSlice.reducer
 
