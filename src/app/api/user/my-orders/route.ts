29df8d1fca7eedfbc6687a8dbc4f16d663285c1d
Susan060 <sushantadhikari060@gmail.com>
2025-12-26 20:34:15 +0545

    Creating My orders Page For User

---
+++ src/app/api/user/my-orders/route.ts
@@ -0,0 +1,27 @@
+import connectDb from "@/lib/db";
+import {auth} from "@/auth"
+import { NextRequest,NextResponse } from "next/server";
+import Order from "@/models/order.model";
+
+export async function GET(req:NextRequest) {
+    try {
+        await connectDb()
+        const session=await auth()
+        const orders=await Order.find({user:session?.user?.id}).populate("user")
+        if(!orders)
+        {
+            return NextResponse.json(
+                {message:"Orders Not Found"},
+                {status:400}
+            )
+        }
+        return NextResponse.json(orders,{status:200})
+
+    } catch (error) {
+        return NextResponse.json(
+            {message:`Get all orders Error${error}`},
+            {status:500}
+        )
+    }
+    
+}
