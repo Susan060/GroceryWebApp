ece429eac268bd0d9ffa343d039b509ebaf28861
Susan060 <sushantadhikari060@gmail.com>
2025-12-31 18:19:24 +0545

    Creating Assignment accept api

---
+++ src/app/api/delivery/assignment/[id]/accept-assignment/route.ts
@@ -0,0 +1,80 @@
+import { auth } from "@/auth";
+import connectDb from "@/lib/db";
+import DeliveryAssignment from "@/models/deliveryAssignmentModel";
+import Order from "@/models/order.model";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest, { params }: { params: { id: string } }) {
+    try {
+        await connectDb()
+        const { id } = await params
+        const session = await auth()
+        const deliveryBoyId = session?.user?.id
+        if (!deliveryBoyId) {
+            return NextResponse.json(
+                { message: "Unauthorized" },
+                { status: 400 }
+            )
+        }
+        const assignment = await DeliveryAssignment.findById(id)
+        if (!assignment) {
+            return NextResponse.json(
+                { message: "Assignment Not found" },
+                { status: 400 }
+            )
+        }
+        if (assignment.status !== "brodcasted") {
+            return NextResponse.json(
+                { message: "Assignment Expired" },
+                { status: 400 }
+            )
+        }
+        const alreadyAssigned = await DeliveryAssignment.findOne({
+            assignedTo: deliveryBoyId,
+            status: { $nin: ["brodcasted", "completed"] }
+        })
+        if (alreadyAssigned) {
+            return NextResponse.json(
+                { message: "Already Assigned to the Order" },
+                { status: 400 }
+            )
+        }
+
+        assignment.assignedTo = deliveryBoyId
+        assignment.status = "assigned"
+        assignment.acceptedAt = new Date()
+        await assignment.save()
+
+        const order = await Order.findById(assignment.order)
+        if (!order) {
+            return NextResponse.json(
+                { message: "Order Not found" },
+                { status: 400 }
+            )
+        }
+        order.assignedDeliveryBoy = deliveryBoyId
+        order.save()
+
+        await DeliveryAssignment.updateMany(
+            {
+                _id: { $ne: assignment._id },
+                brodcastedTo: deliveryBoyId,
+                status: "brodcasted"
+            },
+            {
+                $pull: { brodcastedTo: deliveryBoyId }
+            })
+
+        return NextResponse.json(
+            { message: "Order Accepted Successfully" },
+            { status: 200 }
+        )
+
+    } catch (error) {
+        return NextResponse.json(
+            { message: `accept assignment ${error}` },
+            { status: 500 }
+        )
+    }
+
+}
--- src/components/DeliveryBoyDashboard.tsx
+++ src/components/DeliveryBoyDashboard.tsx
@@ -19,14 +19,22 @@ function DeliveryBoyDashboard() {
         }
         fetchAssignments()
     }, [])
-    useEffect(():any=>{
-        const socket=getSocket()
-        socket.on("new-assignment",(deliveryAssignment)=>{
-            setAssignments((prev)=>[...prev,deliveryAssignment])
+    useEffect((): any => {
+        const socket = getSocket()
+        socket.on("new-assignment", (deliveryAssignment) => {
+            setAssignments((prev) => [...prev, deliveryAssignment])
 
         })
-        return() => {socket.off("new-assignment")}
-    },[])
+        return () => { socket.off("new-assignment") }
+    }, [])
+    const handleAccept = async (id: string) => {
+        try {
+            const result = await axios.get(`/api/delivery/assignment/${id}/accept-assignment`)
+            console.log(result)
+        } catch (error) {
+            console.log(error)
+        }
+    }
     return (
         <div className='w-full min-h-screen bg-gray-50 p-4'>
             <div className='max-w-3xl mx-auto'>
@@ -36,7 +44,8 @@ function DeliveryBoyDashboard() {
                         <p ><b>Order Id </b> #{a?.order._id.slice(-6)}</p>
                         <p className='text-gray-600'>{a.order.address.fullAddress}</p>
                         <div className='flex gap-3 mt-4'>
-                            <button className='flex-1 bg-green-600 text-white py-2 rounded-lg'>Accept</button>
+                            <button className='flex-1 bg-green-600 text-white py-2 rounded-lg'
+                                onClick={() => handleAccept(a._id)}>Accept</button>
                             <button className='flex-1 bg-red-600 text-white py-2 rounded-lg'>Reject </button>
                         </div>
 
