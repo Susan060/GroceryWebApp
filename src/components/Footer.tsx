d0aade1d52f2b1f70b83b850848ba6369dfee3ee
Susan060 <sushantadhikari060@gmail.com>
2026-01-14 14:01:03 +0545

    Final Push

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@
 import React from 'react'
 import { motion } from 'motion/react'
 import Link from 'next/link'
-import { Facebook, Github, Instagram, Linkedin, Mail, MapPin, Phone, Twitter } from 'lucide-react'
+import { Facebook, Github, Instagram, Linkedin, LinkedinIcon, Mail, MapPin, Phone, Twitter } from 'lucide-react'
 
 
 function Footer() {
@@ -13,7 +13,7 @@ function Footer() {
             viewport={{ once: true, amount: 0.3 }}
             transition={{ duration: 0.6, ease: "easeOut" }}
             className='bg-linear-to-r from-green-600 to-green-700 text-white mt-20 '>
-            <div className='w-[90%] md:w-[80%] mx-auto py-10 grid grid-cols-1 md:grid-cols-3 gap-10
+            <div className='w-[90%] md:w-[80%] mx-auto py-10 grid grid-cols-1 md:grid-cols-4 gap-10
     border-b border-green-500/40'>
                 <div>
                     <h2 className="text-2xl font-bold mb-3">GroceryGo</h2>
@@ -29,6 +29,16 @@ function Footer() {
                         <li><Link href={"/user/my-orders"} className='hover:text-white transition'>My Orders</Link></li>
                     </ul>
                 </div>
+                <div>
+                    <h2 className='text-xl font-semibold mb-3'>UI/UX Designer</h2>
+                    <ul className='space-y-2 text-green-100 text-sm'>
+                        <li><Link href={"/"} className='hover:text-white transition'>Kalyan Gharti Gurung</Link></li>
+                        <li><Link href="https://www.linkedin.com/in/kalyan-gharti-gurung-2b0373326?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app" className='hover:text-white transition'>Linked In </Link></li>
+                        <li><Link href={""} className='hover:text-white transition'>kalyangharti60@gmail.com</Link></li>
+                        <li><Link href="https://github.com/Kalyanghartigurung-git" className='hover:text-white transition'>Kalyanghartigurung-git</Link></li>
+                    </ul>
+                </div>
+
                 <div>
                     <h3 className='text-xl font-semibold mb-3'>Contact Us</h3>
                     <ul className='space-y-2 text-green-100 text-sm'>
@@ -64,7 +74,7 @@ function Footer() {
             </div>
 
             <div className='text-center py-4 text-sm text-green-100 bg-green-800/40'>
-                © {new Date().getFullYear()} <span className="font-semibold">GroceryGo</span> All rights reserved.
+                © {new Date().getFullYear()} <span className="font-semibold">GroceryGo</span> All rights reserved. Developed By Susan Adhikari.
             </div>
 
         </motion.div>
